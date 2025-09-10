05f48040829c14cb714eb73acb0531aa7f287a6f
Dan Theo <dantheo@AheadAhead.local>
2025-09-09 22:43:38 -0600

    Add nodemon for development and update dev script

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-// Entry point for Auth Identity Service
+import express from "express";
 
-console.log("Auth Identity Service running...");
+// Entry point for Auth Identity Service
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+  console.log("Default endpoint has been hit!");
+  res.send("Auth Identity Service is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
